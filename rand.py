e66e2523e4272dd62269027b7f0981e14e15b44f
Minh Nguyen <15853@student.vgu.edu.vn>
2020-04-16 13:46:14 +0700

    Solve multiple size error

--- rand.py
+++ rand.py
@@ -7,15 +7,6 @@ def genData(N, d, dist, **arg):
 	'''
 	Parameters:
 	- N: int. Sample size.
-	- d: int. Vector length.from numpy.random import default_rng
-rng = default_rng()
-
-
-def genData(N, d, dist, **arg):
-
-	'''
-	Parameters:
-	- N: int. Sample size.
 	- d: int. Vector length.
 	- dist and **arg: Distribution name (string) and
 	  corresponding parameters. Read NumPy documentation
@@ -35,15 +26,4 @@ def genData(N, d, dist, **arg):
 	out = getattr(rng, dist)(**arg, size = (N,d))
 
 
-	return out
-	- dist and **arg: Distribution name (string) and
-	  corresponding parameters. Read NumPy documentation
-	  (https://docs.scipy.org/doc/numpy-1.15.1/reference/routines.random.html)
-	  to learn about possible options.
-
-	Return:
-	- out: Drawn samples from desired distribution.
-	'''
-
-	out = getattr(rng, dist)(**arg, size = (N,d))
 	return out
